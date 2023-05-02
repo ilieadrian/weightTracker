4b1a9260613a69b154c8ec4bd375ba920f87d2b0
I A <ilie.adrian1986@gmail.com>
2023-05-02 12:01:48 +0300

    basic modal for delete button added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const trashIcons = document.querySelectorAll('.delete-record');
+const modal = document.getElementById('delete-modal');
+const confirmBtn = document.getElementById('delete-confirm');
+const cancelBtn = document.getElementById('delete-cancel');
+
+// Add event listener to all trash icons
+trashIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+});
+
+// Hide modal when cancel button is clicked
+cancelBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Code to delete record from database when confirm button is clicked
+confirmBtn.addEventListener('click', () => {
+  // Add code to delete record from database here
+    console.log('Record deleted from database');
+    modal.style.display = 'none';
+});
