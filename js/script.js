c9c69a6ef3aeda07070a8395c6f501f15c3fee2d
I A <ilie.adrian1986@gmail.com>
2023-05-08 16:43:37 +0300

    delete conformation modal finished

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 // Delete record modal control
 
-const trashIcons = document.querySelectorAll('.delete-record');
-const modal = document.getElementById('delete-modal');
-const confirmBtn = document.getElementById('delete-confirm');
-const cancelBtn = document.getElementById('delete-cancel');
+const trashIcons = document.querySelectorAll('.delete-record')
+const modal = document.getElementById('delete-modal')
+const confirmBtn = document.getElementById('delete-confirm')
+const cancelBtn = document.getElementById('delete-cancel')
+const closeBtn = document.getElementById ('delete-mark')
 
 // Add event listener to all trash icons
 trashIcons.forEach(icon => {
     icon.addEventListener('click', () => {
-        modal.style.display = 'block';
+        modal.style.display = 'block'
         console.log(trashIcons)
-    });
-});
+    })
+})
 
 // Hide modal when cancel button is clicked
 cancelBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
+    modal.style.display = 'none'
+})
 
 // Code to delete record from database when confirm button is clicked
 confirmBtn.addEventListener('click', () => {
   // Add code to delete record from database here
     console.log('Record deleted from database');
-    modal.style.display = 'none';
-});
+    modal.style.display = 'none'
+})
+
+closeBtn.addEventListener('click', () => {
+    // Add code to delete record from database here
+    console.log('modal closed');
+    modal.style.display = 'none'
+})
 
 // End Delete record modal control
