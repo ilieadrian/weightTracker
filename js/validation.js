0b328e5d4ba5a2e1c67402f12185b5ae7150d3ef
I A <ilie.adrian1986@gmail.com>
2024-01-18 17:30:30 +0200

    added * on required fileds, number fields is set as number now. Basic JS validation added

--- js/validation.js
+++ js/validation.js
@@ -1,44 +1,38 @@
-const submitBtn = document.getElementById('submit-btn')
+const date = document.getElementById('date-timepicker').value;
+const weight = document.getElementById('weight').value.trim();
+const submitBtn = document.getElementById('submit-btn');
+const recordsList = document.querySelector('ul');
 const form = document.getElementById('form-id');
-// const date = document.getElementById('date-timepicker').valueAsDate;
-// const weight = document.getElementById('weight').value;
 const comment = document.getElementById('comment').value;
 
 
 function checkRequired() {
-    const date = document.getElementById('date-timepicker').value;
-    const weight = document.getElementById('weight').value;
+    const formElementsIDs = ['date-timepicker', 'weight'];
+    let hasErrors = false;
 
-    const dateInput = document.getElementById('date-timepicker');
-    const weightInput = document.getElementById('weight');
+    formElementsIDs.forEach(elementId => {
+        const inputElement = document.getElementById(elementId);
+        const errorTxtElement = document.getElementById(`${elementId}-form-error`);
+        const value = inputElement.value.trim();
 
-    if (weight.length === 0 && date.length === 0) {
-        error('Both date and weight are required.', dateInput, weightInput);
-    } else if (weight.length === 0) {
-        error('Weight is required.', weightInput);
-    } else if (date.length === 0) {
-        error('Date is required.', dateInput);
-    } else {
-        success(weightInput); // Choose one of the input fields for success
+        if (value.length === 0) {
+            inputElement.classList.add("error");
+            errorTxtElement.classList.add("error");
+            hasErrors = true;
+        } else {
+            inputElement.classList.remove("error");
+            errorTxtElement.classList.remove("error");
+        }
+    });
+
+    if (!hasErrors) {
+        succes(date, weight);
     }
 }
 
-function error(errorMessage, ...inputs) {
-    console.log("Error: " + errorMessage, ...inputs);
-    inputs.forEach(input => {
-        const formControl = input.parentElement;
-        const spanEl = formControl.querySelector('.form-error');
-        spanEl.textContent = errorMessage;  // Set the error message
-        spanEl.classList.remove("hidden");
-    });
+function succes() {
+    console.log("Succes! No errors.");
 }
-
-function succes(input) {
-    console.log("Succes")
-    // const formControl = input.parentElement;
-    // formControl.className = 'form-control';
-}
-
 
 submitBtn.addEventListener('click', e => {
     e.preventDefault()
