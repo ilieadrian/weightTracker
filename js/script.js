301ea8a1e7c10cadfbf44fc560557846bd7fc0aa
I A <ilie.adrian1986@gmail.com>
2024-02-26 21:00:22 +0200

    implement the updateDisplay function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
+
 function handleData(date, weight, comment) {
-    // Your logic for handling valid data goes here
-    console.log('Handling valid data:', date, weight, comment);
-    // let evolution = ;
     let dataArray = JSON.parse(localStorage.getItem('dataArray')) || [];
+
+    console.log('Handling valid data:', date, weight, comment);
 
     const newData = {
         date: date,
@@ -11,10 +11,45 @@ function handleData(date, weight, comment) {
         comment: comment,
     };
 
-    dataArray.push(newData)
-    console.log(dataArray)
-    return { dataArray }
+    dataArray.push(newData);
+
+    localStorage.setItem('dataArray', JSON.stringify(dataArray));
+
+    updateDisplay(dataArray);
+
+    return { dataArray };
+}
+
+function updateDisplay(dataArray) {
     
+    if (dataArray.length > 0) {
+        table.innerHTML = ""; // Clear the table only if there is data
+        dataArray.forEach(function(data, index) {
+            let listItem = document.createElement("li");
+            listItem.classList = "table-row";
+            listItem.setAttribute("table-row-index", index); 
+            // <p id="row-id" class="table-item">${data.rowId}</p>
+            listItem.innerHTML = `
+                <p class="table-item">${data.date}</p>
+                <p class="table-item">${data.weight}</p>
+                <i class="fa-solid ${data.evolution} table-item"></i>
+                <div class="icons table-item">
+                    <i class="tooltip ${data.comment.length > 0 ? "fa-solid" : 'fa-regular'} fa-sharp fa-comment">
+                        <span class="tooltiptext">${data.comment.length > 0 ? data.comment : 'No comment added'}</span>
+                    </i>
+                    <i class="fa-sharp fa-solid fa-trash delete-record" id="delete-${index}"></i>
+                </div>
+            `;
+            table.appendChild(listItem);
+
+            let deleteBtn = document.getElementById(`delete-${index}`);
+            const itemIndex = listItem.getAttribute('table-row-index');
+
+            deleteBtn.addEventListener('click', function() {
+                renderModal(itemIndex);
+            });
+        });
+    } 
 }
 
 function checkEvolution(weight, dataArray) {
