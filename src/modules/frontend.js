e93ce866ea569a644a134deaf3413991f7496388
I A <ilie.adrian1986@gmail.com>
2025-05-07 10:11:09 +0300

    added checkemailToReset function in login.js with basic functionality to check emaiul, then call forgitpassWord function

--- src/modules/frontend.js
+++ src/modules/frontend.js
@@ -59,8 +59,9 @@ function generateLoginUi() {
             <input type="password" name="password" id="signInPassword" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
           </div>
         </div>
-        ${createPasswordResetModal()}
         <div id="loginDivId" class="hidden" role="alert"></div>
+                
+
 
         <div>
           <button id="signInButton" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
@@ -77,7 +78,7 @@ function generateLoginUi() {
 <!--Signup section-->
   <div id="signup" class="hidden flex flex-col space-y-6">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-     <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
       <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create a new account</h2>
     </div>
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -119,7 +120,7 @@ function generateLoginUi() {
     </div>
   </div>
 </div>
-        
+${createPasswordResetModal()}
 `;
 
   container.innerHTML = html;
@@ -132,11 +133,11 @@ function createPasswordResetModal(){
       <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
           <div class="relative p-4 w-full max-w-md max-h-full">
               <!-- Modal content -->
-              <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+              <div class="relative p-4 bg-white rounded-lg shadow-sm dark:bg-gray-700">
                   <!-- Modal header -->
                   <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                       <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                          Create New Product
+                          Enter account email address
                       </h3>
                       <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                           <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -149,31 +150,12 @@ function createPasswordResetModal(){
                   <form class="p-4 md:p-5">
                       <div class="grid gap-4 mb-4 grid-cols-2">
                           <div class="col-span-2">
-                              <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                              <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                              <label for="reset-email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                              <input type="email" name="reset-email" id="reset-email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter email" required>
                           </div>
-                          <div class="col-span-2 sm:col-span-1">
-                              <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                              <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
-                          </div>
-                          <div class="col-span-2 sm:col-span-1">
-                              <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                              <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                  <option selected="">Select category</option>
-                                  <option value="TV">TV/Monitors</option>
-                                  <option value="PC">PC</option>
-                                  <option value="GA">Gaming/Console</option>
-                                  <option value="PH">Phones</option>
-                              </select>
-                          </div>
-                          <div class="col-span-2">
-                              <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                              <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>                    
-                          </div>
-                      </div>
-                      <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                          <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                          Add new product
+                        </div>
+                      <button id="email-pswd-rest-btn" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          Submit
                       </button>
                   </form>
               </div>
@@ -183,6 +165,5 @@ function createPasswordResetModal(){
 
   return html;
 }
-
 
 generateLoginUi();
