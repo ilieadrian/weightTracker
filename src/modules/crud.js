edc25066876f57d3ff1c42acbc151368835e0a91
I A <ilie.adrian1986@gmail.com>
2025-06-06 16:44:50 +0300

    updates

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -18,26 +18,25 @@ export function setupCrudListeners() {
 function getClickedElement(e){
   const container = e.target.closest(".crud-container");
 
-
     if (container) {
-      console.log("Clicked inside .crud-container", container);
+      // console.log("Clicked inside .crud-container", container);
 
       if (e.target.id.startsWith("edit-")) {
         const id = e.target.id.replace("edit-", "");
         getCollection(id)
 
-        console.log("Edit clicked", id);
+        // console.log("Edit clicked", id);
       }
 
       if (e.target.id.startsWith("remove-")) {
         const id = e.target.id.replace("remove-", "");
-        console.log("Remove clicked", id);
+        // console.log("Remove clicked", id);
       }
     }
 }
 
 async function getCollection(id){
-     const weightDocRef = doc(db, "users", userUid, "weights", id);
+    const weightDocRef = doc(db, "users", userUid, "weights", id);
 
  try {
     const docSnap = await getDoc(weightDocRef);
@@ -50,10 +49,6 @@ async function getCollection(id){
   } catch (error) {
     console.error("Error getting document:", error);
   }
-
-  
-    // const weightsCollectionRef = collection(db, "users", userUid, "weights");
-    //   console.log("getCollection(id)", weightsCollectionRef);
     
 }
 
