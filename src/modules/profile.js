0ca4351426f76c3d2129ab64f076a0db93d6ec80
I A <ilie.adrian1986@gmail.com>
2025-07-22 14:25:17 +0300

    moved change email logic in the dedicated section in the accordion menu

--- src/modules/profile.js
+++ src/modules/profile.js
@@ -146,8 +146,26 @@ function generateProfileUI() {
   </h2>
   <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-      <p class="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+        <!-- Change Email -->
+      <form id="change-email-form" class="space-y-4">
+        <div id="notification-container"></div>
+        <div>
+          <label for="new-email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+          <input type="email" id="new-email" required placeholder="Change email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+            focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+        <button type="submit" id="update-email-btn"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
+          font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 
+          focus:outline-none dark:focus:ring-blue-800">
+          Update Email
+        </button>
+
+      </form>
+    
     </div>
   </div>
   <h2 id="accordion-collapse-heading-3">
@@ -179,25 +197,7 @@ function generateProfileUI() {
 
 
 
-      <!-- Change Email -->
-      <form id="change-email-form" class="space-y-4">
-        <div id="notification-container"></div>
-        <div>
-          <label for="new-email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-          <input type="email" id="new-email" required placeholder="Change email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
-            focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
-            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-        </div>
-        <button type="submit" id="update-email-btn"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
-          font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 
-          focus:outline-none dark:focus:ring-blue-800">
-          Update Email
-        </button>
-
-      </form>
+      
 
       <!-- Change Password -->
       <!--<div class="pt-4 border-t border-gray-200 dark:border-gray-700"> Change name
